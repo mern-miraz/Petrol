a5f29a81267fe9f1530d7b7b4cee2a2df862cafc
Miraz <w3miraj@gmail.com>
2024-04-24 22:07:44 +0600

    project

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,7 @@ const Map = () => {
   return (
     <section className='  '>
         <div className=" w-full xl:h-[450px] h-[300px] ">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3627.9240808975633!2d88.64787247606905!3d24.591816155953342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fb91f4ea33d6e1%3A0x390166244f0ce670!2sKeshorHat%20Rd%2C%20Royghati!5e0!3m2!1sen!2sbd!4v1713966938225!5m2!1sen!2sbd" className='w-full h-full '></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193596.1310970019!2d-74.29149231068243!3d40.697329001656875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1713974761570!5m2!1sen!2sbd" className='h-full w-full'></iframe>
         </div>
     </section>
   )
